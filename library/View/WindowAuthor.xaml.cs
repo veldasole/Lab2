3cdf0b09928675d71876c22093a22ab40c373890
veldasole <92044222+veldasole@users.noreply.github.com>
2021-10-31 20:03:36 +0300

    Add files via upload

---
+++ library/View/WindowAuthor.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows;
+using library.ViewModel;
+
+namespace library.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowAuthor.xaml
+    /// </summary>
+    public partial class WindowAuthor : Window
+    {
+        public WindowAuthor()
+        {
+            InitializeComponent();
+            AuthorViewModel vmAuthor = new AuthorViewModel();
+            lvAuthor.ItemsSource = vmAuthor.ListAuthor;
+        }
+    }
+}
---
+++ library/View/WindowPublish.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+    using System.Windows;
+    using library.ViewModel;
+
+    namespace library.View
+    {
+        /// <summary>
+        /// Логика взаимодействия для WindowPublish.xaml
+        /// </summary>
+        public partial class WindowPublish : Window
+        {
+            public WindowPublish()
+            {
+                InitializeComponent();
+                PublishViewModel vmPublish = new PublishViewModel();
+                lvPublish.ItemsSource = vmPublish.ListPublish;
+            }
+        }
+    }
